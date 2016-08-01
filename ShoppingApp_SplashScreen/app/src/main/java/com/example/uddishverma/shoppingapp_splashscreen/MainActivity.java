ea6b3c8dd3102b76e89bd4eac1c490e901f7e5c2
Uddish Verma <uddishverma22@gmail.com>
2016-08-02 02:12:02 +0530

    Commit-1, adding splash screen

---
+++ ShoppingApp_SplashScreen/app/src/main/java/com/example/uddishverma/shoppingapp_splashscreen/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.uddishverma.shoppingapp_splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        int secondsDelayed = 1;
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(this, Splash.class));
+//                finish();
+//            }
+//        });
+
+    }
+}
