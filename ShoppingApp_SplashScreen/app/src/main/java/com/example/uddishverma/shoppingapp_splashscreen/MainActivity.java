d22e1b7377e1bf0972eeb6387cdb7e1e9f905c3f
Uddish Verma <uddishverma22@gmail.com>
2016-08-02 22:28:22 +0530

    second commit- adding basic toolbar

--- ShoppingApp_SplashScreen/app/src/main/java/com/example/uddishverma/shoppingapp_splashscreen/MainActivity.java
+++ ShoppingApp_SplashScreen/app/src/main/java/com/example/uddishverma/shoppingapp_splashscreen/MainActivity.java
@@ -5,23 +5,19 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        int secondsDelayed = 1;
-//
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                startActivity(new Intent(this, Splash.class));
-//                finish();
-//            }
-//        });
-
+        //Adding toolbar in the main activity
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
     }
 }
