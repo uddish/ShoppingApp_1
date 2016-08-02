167462f144a31e0600ee320ca26e23bd079038c7
Uddish Verma <uddishverma22@gmail.com>
2016-08-03 01:07:51 +0530

    3rd commmit, Adding options on Toolbar and handling click events

--- ShoppingApp_SplashScreen/app/src/main/java/com/example/uddishverma/shoppingapp_splashscreen/MainActivity.java
+++ ShoppingApp_SplashScreen/app/src/main/java/com/example/uddishverma/shoppingapp_splashscreen/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
         //Adding toolbar in the main activity
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int resId = item.getItemId();
+        if(resId == R.id.action_search) {
+            Toast.makeText(MainActivity.this, "Search Selected", Toast.LENGTH_SHORT).show();
+        }
+        if(resId == R.id.action_settings)   {
+            Toast.makeText(MainActivity.this, "Settings Selected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
