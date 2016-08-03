256db7389f431983e090842141bfc1554fca38d2
Uddish Verma <uddishverma22@gmail.com>
2016-08-03 20:57:02 +0530

    custom colors according to the material design

---
+++ MaterialColorPallete/app/src/main/java/com/example/uddishverma/materialcolotpalettes/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.uddishverma.materialcolotpalettes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+        setSupportActionBar(toolbar);
+    }
+}
