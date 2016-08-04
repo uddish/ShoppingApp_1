c4a37e7bfdd0909e0b09d1ad8c3bba12a62edb18
Uddish Verma <uddishverma22@gmail.com>
2016-08-04 19:24:31 +0530

    Floating action Button

---
+++ FloatingActionBar/app/src/main/java/com/example/uddishverma/floatingactionbar/FabActivityResult.java
@@ -0,0 +1,13 @@
+package com.example.uddishverma.floatingactionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FabActivityResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_activity_result);
+    }
+}
